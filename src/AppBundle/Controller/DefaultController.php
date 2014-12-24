784775792c7e9954f137a70f348f6dee944ed407
yboker <8236138@qq.com>
2014-12-24 23:29:33 +0800

    add

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,4 +14,14 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+    /**
+     * @Route("/hello/{name}.{_formatxx}", defaults={"_formatxx"="html"}, name="hello")
+     */
+    public function helloAction($name, $_formatxx)
+    {
+        //echo 'ok';
+        //return ;
+        return $this->render('default/hello.' . $_formatxx . '.twig', array('name'=>$name));
+    }
+
 }
