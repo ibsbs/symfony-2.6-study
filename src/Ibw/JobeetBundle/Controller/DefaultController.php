d8c0f368a21c66eb748dc904f1ddbbe250ec3677
yboker <8236138@qq.com>
2014-12-29 17:02:45 +0800

    add ladybug

--- src/Ibw/JobeetBundle/Controller/DefaultController.php
+++ src/Ibw/JobeetBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace Ibw\JobeetBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
@@ -10,8 +12,19 @@ class DefaultController extends Controller
     {
         return $this->render('IbwJobeetBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    /**
+     * @Route("/jobeet/test/")
+     */
     public function testAction()
     {
-        $this->getRequest()->get('post');
+        $request = $this->getRequest();
+        ladybug_dump($request);
+//        $dumper = new \Ladybug\Dumper();
+//        $dumper->setFormat('json');
+//        $json = $dumper->dump('1');
+//        var_dump($json);
+        $response = new Response('hello world!');
+        return $response;
     }
 }
